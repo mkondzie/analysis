589dc71cff6d97a0d02d9a5fd01809f8a22388d1
mk <m.kondzielsk@student.uw.edu.pl>
2024-03-16 19:16:07 +0100

    single file

---
+++ FVAnalysis_sk4_start.c
@@ -0,0 +1,189 @@
+#define FVAnalysis_sk4_start_cxx
+#include "FVAnalysis_sk4_start.h"
+#include <TH2.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+using namespace std;
+
+//#include "Event.h"
+
+   // 0 data files
+   // >0 MC files
+   // 1 all MC
+   // 2 vue CC
+   //3  bar nue CC
+   // 4 numu CC
+   //5 bar numu CC
+   // 6 NC all
+// may be added
+   //7 nutau CC
+   // 8 bar nu tau CC
+   //
+
+FVAnalysis_sk4_start *E = new FVAnalysis_sk4_start();
+//E->SetVersion(4); // for SK4, 3- sk3 etc
+//E->SetDatatype(2); //0- fcData, 2 fcMC
+
+//const int analysed_files=1; //0 data, 1 mc change in the FVAnalysis_sk4.h
+
+//numbers taken from ~/osc/Card/sk.16b/standard/q13-fixed.full.nh.card
+
+const double mc_livetime=182625.0; //eq 500years sk4
+const double data_livetime=3244.4;// sk4 fcdata
+
+
+void FVAnalysis_sk4_start::Loop()
+{
+    //   In a ROOT session, you can do:
+    //      root> .L FVAnalysis_sk4_start.C
+    //      root> FVAnalysis_sk4_start t
+    //      root> t.GetEntry(12); // Fill t data members with entry number 12
+    //      root> t.Show();       // Show values of entry 12
+    //      root> t.Show(16);     // Read and show values of entry 16
+    //      root> t.Loop();       // Loop on all entries
+    //
+    
+    //     This is the loop skeleton where:
+    //    jentry is the global entry number in the chain
+    //    ientry is the entry number in the current Tree
+    //  Note that the argument to GetEntry must be:
+    //    jentry for TChain::GetEntry
+    //    ientry for TTree::GetEntry and TBranch::GetEntry
+    //
+    //       To read only selected branches, Insert statements like:
+    // METHOD1:
+    //    fChain->SetBranchStatus("*",0);  // disable all branches
+    //    fChain->SetBranchStatus("branchname",1);  // activate branchname
+    // METHOD2: replace line
+    //    fChain->GetEntry(jentry);       //read all branches
+    //by  b_branchname->GetEntry(ientry); //read only this branch
+    
+    gROOT->Time();
+    gROOT->Reset();
+    
+    //some extras
+    gROOT->SetStyle("Plain");
+    gStyle->SetPalette(1, 0);
+    gStyle->SetOptStat("e");	//prints only number of events, underflows and overflows
+    
+    //gStyle->SetOptStat(0);
+    gStyle->SetOptFit(111);
+    gStyle->SetTitleSize(0.07, "X");
+    gStyle->SetTitleSize(0.07, "Y");
+    gStyle->SetLabelSize(0.05, "X");
+    gStyle->SetLabelSize(0.05, "Y");
+    gStyle->SetPadTopMargin(0.15);
+    gStyle->SetPadLeftMargin(0.15);
+    gStyle->SetPadRightMargin(0.05);
+    gStyle->SetPadBottomMargin(0.2);
+    gStyle->SetTitleFontSize(0.08);
+    //gStyle->SetTitleX(0.4);
+    gStyle->SetTitleFontSize(0.08);
+    gStyle->SetMarkerSize(1.0);
+    //gStyle->SetTitle0ffset(1.5, "Y");
+    gStyle->SetPaintTextFormat("2.2f");
+    gStyle->SetFitFormat("2.2f");
+    gStyle->SetStatFormat("2.2f");
+    
+    
+    Char_t run[300];
+    sprintf(run,"%s_fc_data_mc_all",sk_per);
+    
+    Char_t input[300];
+    sprintf(input,"%s_fc_data_mc_all", sk_per);
+    //OUTPUT ROOT
+    
+    
+    
+    // OUTPUT PDF
+    Char_t outfile_data[300];
+    sprintf(outfile_data, "/home/mk/Documents/SK/analysis/outputData/APFIT_momentum_data_%s.pdf",run);
+    
+    Char_t outfile_mc[300];
+    sprintf(outfile_mc, "/home/mk/Documents/SK/analysis/outputMC/APFIT_momentum_mc_%s.pdf",run);
+    
+    
+    
+    
+    Int_t nbins_mom=200;
+    Float_t min_p=0;
+    Float_t max_p=2000;
+    
+    
+    TH1F *mom_data = new TH1F("mom_data","data", nbins_mom, min_p,max_p);
+    TH1F *mom_mc = new TH1F("mom_mc","MC", nbins_mom, min_p,max_p);
+    
+    mom_mc->GetXaxis()->SetNdivisions(505, 4);
+	//mom_mc->GetXaxis()->SetTitle("z (mm)");
+	//mom_mc->GetYaxis()->SetTitle("counts");
+	mom_mc->GetYaxis()->SetNdivisions(409);
+	mom_mc->GetXaxis()->SetLabelSize(0.04);
+	mom_mc->GetYaxis()->SetLabelSize(0.04);
+	mom_mc->GetYaxis()->SetLabelOffset(0.005);
+	mom_mc->GetXaxis()->SetLabelOffset(0.005);
+	mom_mc->GetXaxis()->CenterTitle(true);
+	mom_mc->GetYaxis()->CenterTitle(true);
+	mom_mc->GetXaxis()->SetTitleOffset(1.2);
+        
+    if (fChain == 0) return;
+    
+    
+    Long64_t nentries = fChain->GetEntries();
+    cout<<" number of nentries "<< nentries<<endl;
+    
+    
+    
+    
+    
+    double ratio= data_livetime/mc_livetime;
+    cout<<"ratio data/mc  = "<<ratio<<endl;
+    
+    // TCut eSingleRingQuasiElasticCut = "(((osc_tuple.nring == 1) && (osc_tuple.ip == 2)) && ((osc_tuple.mode == 1) || (osc_tuple.mode == -1)))";
+    // TTreeFormula selectedTreeFormula("eSingleRingQuasiElasticSelected",eSingleRingQuasiElasticCut,fChain);
+
+
+    for (Long64_t i=0; i<nentries;i++)
+    {
+        
+        
+        E->GetEntry(i);
+        
+        if(analysed_files==0) //data files
+        {
+     
+        E->GetEntry(i);
+        
+        if(analysed_files==0) //data files
+        {
+            
+                mom_data->Fill(E->amom);
+           
+            
+            
+        }
+           
+            
+            
+            if(analysed_files==1)//MC files
+            {
+                
+                    mom_mc->Fill(E->amom);
+            }
+            
+        
+   
+    }}
+    
+ TCanvas *c1 = new TCanvas("c1", "Momentum -data", 15,15,900,900);
+    
+    c1->SetLeftMargin(0.05);
+    c1->SetRightMargin(0.05);
+    c1->SetTopMargin(0.05);
+    c1->SetBottomMargin(0.05);
+    c1->SetFillColor(kWhite);
+    c1->Draw();
+    
+    c1->cd();
+    mom_data->Draw();
+    c1->Print(outfile_data);
+}
---
+++ FVAnalysis_sk4_start.h
@@ -0,0 +1,400 @@
+ //////////////////////////////////////////////////////////
+// This class has been automatically generated on
+// Sun Mar 15 22:55:21 2020 by ROOT version 6.12/04
+// from TChain osc_tuple/
+//////////////////////////////////////////////////////////
+
+#ifndef FVAnalysis_sk4_start_h
+#define FVAnalysis_sk4_start_h
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+//#include "global.h"
+#include <iostream>
+using namespace std;
+
+const int nHistos = 10;
+const int analysed_files=0;
+
+//for FVanalysis_reaction_types_sk4.c option 0 for data and 1 for MC and NHistos = 10
+//for FVanalysis_sk4.c add option 2 tau neutrinos and Nhsitos=9
+
+string sk_period("sk4");
+
+Char_t sk_per[200]={"sk4"};
+
+// Header file for the classes stored in the TTree if any.
+
+class FVAnalysis_sk4_start {
+public :
+   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+   Int_t           fCurrent; //!current Tree number in a TChain
+    
+    
+    
+    
+    
+     
+    
+    
+// Fixed size dimensions of array or collections stored in the TTree if any.
+
+   // Declaration of leaf types
+   Int_t           nn;
+   Int_t           nn_mctruth;
+   Int_t           nring;
+   Int_t           ipnu;
+   Float_t         dirnu[3];
+   Float_t         pnu;
+   Int_t           mode;
+   Int_t           ip;
+   Float_t         dprob;
+   Float_t         dir[3];
+    Float_t        pos[3];
+   Float_t         amom;
+   Float_t         path;
+   Float_t         wall;
+   Int_t           itype;
+   Int_t           muedk;
+   Float_t         flxg[3];
+   Float_t         flxgo[3];
+   Float_t         flxh[3];
+   Float_t         flxho[3];
+   Float_t         weightx;
+    Float_t        ringc;
+    Float_t        probms[5];   //[nring] single ring
+    Float_t        prmslg[10][5];   //[nring] multiring
+   Float_t         ra;
+   Float_t         dec;
+   Float_t         time[3];
+   Float_t         date[3];
+   Float_t         oscweight3f;
+   Float_t         cp1p2oscweight3f;
+   Float_t         cp3p2oscweight3f;
+   Float_t         invoscweight3f;
+   Float_t         true_lepmom;
+   Float_t         true_lepdir[3];
+
+   // List of branches
+   TBranch        *b_nn;   //!
+   TBranch        *b_nn_mctruth;   //!
+   TBranch        *b_nring;   //!
+   TBranch        *b_ipnu;   //!
+   TBranch        *b_dirnu;   //!
+   TBranch        *b_pnu;   //!
+   TBranch        *b_mode;   //!
+   TBranch        *b_ip;   //!
+   TBranch        *b_dprob;   //!
+   TBranch        *b_dir;   //!
+   TBranch        *b_pos;   //!
+   TBranch        *b_amom;   //!
+   TBranch        *b_path;   //!
+   TBranch        *b_wall;   //!
+   TBranch        *b_itype;   //!
+   TBranch        *b_muedk;   //!
+   TBranch        *b_flxg;   //!
+   TBranch        *b_flxgo;   //!
+   TBranch        *b_flxh;   //!
+   TBranch        *b_flxho;   //!
+   TBranch        *b_weightx;   //!
+    TBranch        *b_ringc;   //!
+    TBranch        *b_probms;
+    TBranch        *b_prmslg;
+   TBranch        *b_ra;   //!
+   TBranch        *b_dec;   //!
+   TBranch        *b_time;   //!
+   TBranch        *b_date;   //!
+   TBranch        *b_oscweight3f;   //!
+   TBranch        *b_cp1p2oscweight3f;   //!
+   TBranch        *b_cp3p2oscweight3f;   //!
+   TBranch        *b_invoscweight3f;   //!
+   TBranch        *b_true_lepmom;   //!
+   TBranch        *b_true_lepdir;   //!
+
+   FVAnalysis_sk4_start(TTree *tree=0);
+   // FVAnalysis_sk4 *E = new FVAnalysis_sk4();
+    
+   virtual ~FVAnalysis_sk4_start();
+   virtual Int_t    Cut(Long64_t entry);
+   virtual Int_t    GetEntry(Long64_t entry);
+   virtual Long64_t LoadTree(Long64_t entry);
+   virtual void     Init(TTree *tree);
+   virtual void     Loop();
+   virtual Bool_t   Notify();
+   virtual void     Show(Long64_t entry = -1);
+    
+    enum DataTypes{
+        fcData,
+        pcData,
+        fcMC,
+        pcMC,
+        upmuMC,
+        
+        SK1fcData,
+        SK1pcData,
+        SK1fcMC,
+        SK1pcMC,
+        SK1upmuMC,
+        
+        SK2fcData,
+        SK2pcData,
+        SK2fcMC,
+        SK2pcMC,
+        SK2upmuMC,
+        
+        SK3fcData,
+        SK3pcData,
+        SK3fcMC,
+        SK3pcMC,
+        SK3upmuMC,
+        
+        SK4fcData,
+        SK4pcData,
+        SK4fcMC,
+        SK4pcMC,
+        SK4upmuMC,
+    };
+    
+    enum EventTypes{
+        SubGeVE0dcy,
+        SubGeVE1dcy,
+        SubGeVPi01R,
+        SubGeVMu0dcy,
+        SubGeVMu1dcy,
+        SubGeVMu2dcy,
+        SubGeVPi0MR,
+        MulGeVE,
+        MulGeVMu,
+        MultiRingE,
+        MultiRingMu,
+        PCstop,
+        PCthrough,
+        UPstopMu,
+        NonShoweringMu,
+        ShoweringMu
+    };
+
+    
+    int  _version;  // SK detector version
+    int _datatype;
+    
+    void SetVersion( int x)  { _version = x; }
+    int  GetVersion( void)   { return _version;}
+    
+    int  GetDatatype(void)   { return _datatype;}
+    void SetDatatype(int x)  { _datatype = x;}
+    
+    double weight();
+    
+    int _datalivetime;
+    
+    void  set_datalivetime ( double x ) { _datalivetime = x; cout << _datalivetime << endl;}
+    double get_datalivetime( void ) { return _datalivetime;}
+    
+   
+  
+    
+   
+    
+    
+   };
+
+#endif
+
+#ifdef FVAnalysis_sk4_start_cxx
+FVAnalysis_sk4_start::FVAnalysis_sk4_start(TTree *tree) : fChain(0)
+{
+// if parameter tree is not specified (or zero), connect the file
+// used to generate this class and read the Tree.
+   if (tree == 0 && analysed_files==0)
+   {
+
+    #ifdef SINGLE_TREE
+      // The following code should be used if you want this class to access
+      // a single tree instead of a chain
+      TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/home/mk/Downloads/expanded_fv/data/fcdt.sk4.19b.mrbdt-2020.9.root");
+      if (!f || !f->IsOpen()) {
+         f = new TFile("/home/mk/Downloads/expanded_fv/data/fcdt.sk4.19b.mrbdt-2020.9.root");
+          cout<<"reading one SK4 data file"<<f->GetName()<<endl;
+      }
+      f->GetObject("osc_tuple",tree);
+
+#else // SINGLE_TREE
+
+      // The following code should be used if you want this class to access a chain
+      // of trees.
+      TChain * chain = new TChain("osc_tuple","");
+      chain->Add("/home/mk/Downloads/expanded_fv/data/fcdt.sk4.19b.mrbdt-2020.*.root/osc_tuple");
+
+       cout<<"reading chain SK4 data file "<<chain->GetName()<<endl;
+      tree = chain;
+#endif // SINGLE_TREE
+
+   }
+    
+    if (tree == 0 && analysed_files==1)
+    {
+        
+#ifdef SINGLE_TREE
+        // The following code should be used if you want this class to access
+        // a single tree instead of a chain
+        TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/home/mk/Downloads/expanded_fv/MC/fcmc.sk4.19b.mrbdt-2020.9.root");
+        if (!f || !f->IsOpen()) {
+            f = new TFile("/home/mk/Downloads/expanded_fv/MC/fcmc.sk4.19b.mrbdt-2020.9.root");
+            cout<<"reading one MC file"<<endl;
+        }
+        f->GetObject("osc_tuple",tree);
+        
+#else // SINGLE_TREE
+        
+        // The following code should be used if you want this class to access a chain
+        // of trees.
+        TChain * chain = new TChain("osc_tuple","");
+        chain->Add("/home/mk/Downloads/expanded_fv/MC/fcmc.sk4.19b.mrbdt-2020.*.root/osc_tuple");
+        
+        
+        cout<<"reading chain file for SK4 MC"<<endl;
+        tree = chain;
+#endif // SINGLE_TREE
+        
+    }
+    if (tree == 0 && analysed_files==2)
+        {
+            
+    #ifdef SINGLE_TREE
+            // The following code should be used if you want this class to access
+            // a single tree instead of a chain
+            TFile *f = (TFile*)gROOT->GetListOfFiles()->FindObject("/Users/magdapz/Documents/analiza_sk/ntuple_files/1_2_m_FV/taumc.sk4.19b.skmeeting-202002.9.root");
+            if (!f || !f->IsOpen()) {
+                f = new TFile("/Users/magdapz/Documents/analiza_sk/ntuple_files/1_2_m_FV/taumc.sk4.19b.mrbdt-2020.9.root");
+                cout<<"reading one MC SK4 tau file"<<endl;
+            }
+            f->GetObject("osc_tuple",tree);
+            
+    #else // SINGLE_TREE
+
+
+
+    
+            
+            // The following code should be used if you want this class to access a chain
+            // of trees.
+            TChain * chain = new TChain("osc_tuple","");
+            chain->Add("~magdapz/Documents/analiza_sk/ntuple_files/1_2_m_FV/taumc.sk4.19b.mrbdt-2020.*.root/osc_tuple");
+            
+            
+            cout<<"reading chain file for SK4 tau MC"<<endl;
+            tree = chain;
+    #endif // SINGLE_TREE
+            
+        }
+ 
+   Init(tree);
+}
+
+FVAnalysis_sk4_start::~FVAnalysis_sk4_start()
+{
+   if (!fChain) return;
+   delete fChain->GetCurrentFile();
+}
+
+Int_t FVAnalysis_sk4_start::GetEntry(Long64_t entry)
+{
+// Read contents of entry.
+   if (!fChain) return 0;
+   return fChain->GetEntry(entry);
+}
+Long64_t FVAnalysis_sk4_start::LoadTree(Long64_t entry)
+{
+// Set the environment to read one entry
+   if (!fChain) return -5;
+   Long64_t centry = fChain->LoadTree(entry);
+   if (centry < 0) return centry;
+   if (fChain->GetTreeNumber() != fCurrent) {
+      fCurrent = fChain->GetTreeNumber();
+      Notify();
+   }
+   return centry;
+}
+
+void FVAnalysis_sk4_start::Init(TTree *tree)
+{
+   // The Init() function is called when the selector needs to initialize
+   // a new tree or chain. Typically here the branch addresses and branch
+   // pointers of the tree will be set.
+   // It is normally not necessary to make changes to the generated
+   // code, but the routine can be extended by the user if needed.
+   // Init() will be called many times when running on PROOF
+   // (once per file to be processed).
+
+   // Set branch addresses and branch pointers
+   if (!tree) return;
+   fChain = tree;
+   fCurrent = -1;
+   fChain->SetMakeClass(1);
+
+   fChain->SetBranchAddress("nn", &nn, &b_nn);
+   fChain->SetBranchAddress("nn_mctruth", &nn_mctruth, &b_nn_mctruth);
+   fChain->SetBranchAddress("nring", &nring, &b_nring);
+   fChain->SetBranchAddress("ipnu", &ipnu, &b_ipnu);
+   fChain->SetBranchAddress("dirnu", dirnu, &b_dirnu);
+   fChain->SetBranchAddress("pnu", &pnu, &b_pnu);
+   fChain->SetBranchAddress("mode", &mode, &b_mode);
+   fChain->SetBranchAddress("ip", &ip, &b_ip);
+   fChain->SetBranchAddress("dprob", &dprob, &b_dprob);
+   fChain->SetBranchAddress("dir", dir, &b_dir);
+    fChain->SetBranchAddress("pos", pos, &b_pos);
+   fChain->SetBranchAddress("amom", &amom, &b_amom);
+   fChain->SetBranchAddress("path", &path, &b_path);
+   fChain->SetBranchAddress("wall", &wall, &b_wall);
+   fChain->SetBranchAddress("itype", &itype, &b_itype);
+   fChain->SetBranchAddress("muedk", &muedk, &b_muedk);
+   fChain->SetBranchAddress("flxg", flxg, &b_flxg);
+   fChain->SetBranchAddress("flxgo", flxgo, &b_flxgo);
+   fChain->SetBranchAddress("flxh", flxh, &b_flxh);
+   fChain->SetBranchAddress("flxho", flxho, &b_flxho);
+   fChain->SetBranchAddress("weightx", &weightx, &b_weightx);
+    fChain->SetBranchAddress("ringc", &ringc, &b_ringc);
+    fChain->SetBranchAddress("probms", &probms, &b_probms);
+    fChain->SetBranchAddress("prmslg", &prmslg, &b_prmslg);
+   fChain->SetBranchAddress("ra", &ra, &b_ra);
+   fChain->SetBranchAddress("dec", &dec, &b_dec);
+   fChain->SetBranchAddress("time", time, &b_time);
+   fChain->SetBranchAddress("date", date, &b_date);
+   fChain->SetBranchAddress("oscweight3f", &oscweight3f, &b_oscweight3f);
+   fChain->SetBranchAddress("cp1p2oscweight3f", &cp1p2oscweight3f, &b_cp1p2oscweight3f);
+   fChain->SetBranchAddress("cp3p2oscweight3f", &cp3p2oscweight3f, &b_cp3p2oscweight3f);
+   fChain->SetBranchAddress("invoscweight3f", &invoscweight3f, &b_invoscweight3f);
+   fChain->SetBranchAddress("true_lepmom", &true_lepmom, &b_true_lepmom);
+   fChain->SetBranchAddress("true_lepdir", true_lepdir, &b_true_lepdir);
+   Notify();
+}
+
+Bool_t FVAnalysis_sk4_start::Notify()
+{
+   // The Notify() function is called when a new file is opened. This
+   // can be either for a new TTree in a TChain or when when a new TTree
+   // is started when using PROOF. It is normally not necessary to make changes
+   // to the generated code, but the routine can be extended by the
+   // user if needed. The return value is currently not used.
+
+    cout<<"New file is opened"<<endl;
+   return kTRUE;
+}
+
+void FVAnalysis_sk4_start::Show(Long64_t entry)
+{
+// Print contents of entry.
+// If entry is not specified, print current entry
+   if (!fChain) return;
+   fChain->Show(entry);
+}
+Int_t FVAnalysis_sk4_start::Cut(Long64_t entry)
+{
+// This function may be called from Loop.
+// returns  1 if entry is accepted.
+// returns -1 otherwise.
+   return 1;
+}
+#endif // #ifdef FVAnalysis_sk4_cxx
