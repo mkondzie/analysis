09838176a0ce9b60110df9b66ba87a3c1d4e68ec
mkondzie <141751789+mkondzie@users.noreply.github.com>
2024-04-24 22:07:05 +0200

    Add files via upload

--- tupleReader.C
+++ tupleReader.C
@@ -2,79 +2,141 @@
 #include <TCanvas.h>
 #include <TFile.h>
 #include <TH1F.h>
+#include <TPad.h>
 #include <TStyle.h>
 #include <TTree.h>
-#include <TLine.h>
+#include <TTreeFormula.h>
+#include <TLegend.h>
+#include <TLatex.h>
+// #include <iostream>
 
-
-
-void setPlottingOptions(TH1F *hist, TString title = "; cos#theta; counts") {
+void setPlottingOptions(TH1F *hist, TString title = "; cos#theta; events") {
 
   hist->GetYaxis()->SetNdivisions(505, 4);
   hist->GetXaxis()->SetNdivisions(505, 4);
-  hist->GetXaxis()->SetLabelSize(0.045);
-  hist->GetYaxis()->SetLabelSize(0.043);
-  hist->GetXaxis()->SetTitleSize(0.04);
-  hist->GetYaxis()->SetTitleSize(0.04);
-  hist->GetYaxis()->SetLabelOffset(0.005);
-  hist->GetXaxis()->SetLabelOffset(0.005);
+  hist->GetXaxis()->SetLabelSize(0.05);
+  hist->GetYaxis()->SetLabelSize(0.05);
+  hist->GetXaxis()->SetTitleSize(0.2);
+  hist->GetYaxis()->SetTitleSize(0.2);
+  hist->GetYaxis()->SetLabelOffset(0.003);
+  hist->GetXaxis()->SetLabelOffset(0.004);
   hist->GetXaxis()->CenterTitle(true);
   hist->GetYaxis()->CenterTitle(true);
-  hist->GetXaxis()->SetTitleOffset(1.2);
+  hist->GetXaxis()->SetTitleOffset(0.4);
+  hist->GetYaxis()->SetTitleOffset(0.8);
   hist->SetTitle(title);
+  hist->SetTitleOffset(2);
+  hist->SetMarkerStyle(4);
+  hist->SetMarkerSize(0.8);
   gStyle->SetCanvasColor(kWhite);
   gStyle->SetTitleFontSize();
   gStyle->SetOptStat(0);
 }
 
 TH1F *getAngleDistribution(TTree *tuple,
-                           TString condition = "wall > 100 && nring == 1",
+                           TString condition = "wall > 200 && nring == 1",
                            bool weight = true) {
-  // used to calculate cosine of zenith angle and oscillation weights
+  // use to calculate cosine of zenith angle and oscillation weights
   Float_t dir[3];
   Float_t oscweight3f;
-  // Float_t cp1p2oscweight3f;
-  // Float_t cp3p2oscweight3f;
-  // Float_t invoscweight3f;
+  Float_t weightx;
   tuple->SetBranchAddress("dir", dir);
   tuple->SetBranchAddress("oscweight3f", &oscweight3f);
-  // tuple->SetBranchAddress("cp1p2oscweight3f", &cp1p2oscweight3f);
-  // tuple->SetBranchAddress("cp3p2oscweight3f", &cp3p2oscweight3f);
-  // tuple->SetBranchAddress("invoscweight3f", &invoscweight3f);
+  tuple->SetBranchAddress("weightx", &weightx);
 
-  // used to set cuts
-  // Int_t nring;
-  // Int_t ip;
-  // Int_t ipnu;
-  // Int_t itype;
-  // Int_t mode;
-  // Float_t wall;
-  // Float_t amom;
-  // tuple->SetBranchAddress("nring", &nring);
-  // tuple->SetBranchAddress("ip", &ip);
-  // tuple->SetBranchAddress("ipnu", &ipnu);
-  // tuple->SetBranchAddress("itype", &itype);
-  // tuple->SetBranchAddress("mode", &mode);
-  // tuple->SetBranchAddress("wall", &wall);
-  // tuple->SetBranchAddress("amom", &amom);
+  TH1F *cosZenith = new TH1F("", condition + "; cos#theta; events", 10, -1, 1);
 
-  TH1F *cosZenith = new TH1F("", condition + "; cos#theta; counts", 10, -1, 1);
   // set cuts on cosine of zenith angle distribution
-  Long_t nEntries = tuple->GetEntries(condition);
-  for (Long_t i = 0; i < nEntries; i++) {
+  TTreeFormula *formula = new TTreeFormula("", condition, tuple);
+  Long64_t nEntries = tuple->GetEntries();
+  for (Long64_t i = 0; i < nEntries; ++i) {
     tuple->GetEntry(i);
-    if (weight) {
-      cosZenith->Fill(-dir[2] / std::sqrt(dir[0] * dir[0] + dir[1] * dir[1] +
-                                          dir[2] * dir[2]),
-                      oscweight3f);
-    } else {
-      cosZenith->Fill(-dir[2] / std::sqrt(dir[0] * dir[0] + dir[1] * dir[1] +
-                                          dir[2] * dir[2]),
-                      1);
+    //  evaluate the cut condition using the TTreeFormula
+    if (formula->EvalInstance()) {
+      if (weight) {
+        cosZenith->Fill(-dir[2] / std::sqrt(dir[0] * dir[0] + dir[1] * dir[1] +
+                                            dir[2] * dir[2]),
+                        weightx * oscweight3f);
+      } else {
+        cosZenith->Fill(-dir[2] / std::sqrt(dir[0] * dir[0] + dir[1] * dir[1] +
+                                            dir[2] * dir[2]),
+                        1);
+      }
     }
   }
 
   return cosZenith;
+}
+
+void plotOnePad(TH1F *histData, TH1F *histMC, TH1F *histMCWeight, TString momentumString = "single - ring events") {
+
+  TPad *subpad = new TPad("subpad", "subpad", 0, 0.2, 1, 1);
+  subpad->Draw();
+  subpad->cd();
+  subpad->SetTopMargin(0.055);
+  subpad->SetRightMargin(0.1);
+  subpad->SetLeftMargin(0.1);
+  subpad->SetBottomMargin(0);
+
+  histMCWeight->SetMinimum(histData->GetMinimum() * 0.9);
+  histMCWeight->SetMaximum(histData->GetMaximum() * 1.24);
+  histMCWeight->SetLineColor(kGreen + 2);
+  histMCWeight->Draw("hist");
+
+  histMC->SetMaximum(histData->GetMaximum() * 1.12);
+  histMC->SetLineColor(kRed);
+  histMC->Draw("hist, same");
+
+  histData->SetLineColor(kBlack);
+  histData->SetMarkerStyle(20);
+  histData->Draw("PE, same");
+
+  TLatex latex ;
+  latex.SetNDC ();
+  latex.SetTextSize (0.06);
+  latex.SetTextColor(kBlue + 4);
+  latex.DrawText (0.6 ,0.83 , momentumString );
+}
+
+void plotOneRatio(TH1F *histData, TH1F *histMC, TH1F *histMCWeight,
+                  TString title = "; cos#theta ; data/MC") { 
+
+  TPad *subpadRatio = new TPad("subpadRatio", "subpadRatio", 0, 0, 1, 0.2);
+  subpadRatio->Draw();
+  subpadRatio->cd();
+  subpadRatio->SetTopMargin(0);
+  subpadRatio->SetRightMargin(0.1);
+  subpadRatio->SetLeftMargin(0.1);
+  subpadRatio->SetBottomMargin(0.3);
+
+  TH1F *ratio = (TH1F *)histData->Clone();
+  ratio->Divide(histMC);
+  setPlottingOptions(ratio, title);
+
+  TH1F *ratioWeight = (TH1F *)histData->Clone();
+  ratioWeight->Divide(histMCWeight);
+  setPlottingOptions(ratioWeight, title);
+
+  ratio->SetLineColor(kRed);
+  ratio->GetYaxis()->SetRangeUser(0.5, 1.28);
+  ratio->GetXaxis()->SetLabelSize(0.2);
+  ratio->GetYaxis()->SetLabelSize(0.2);
+  ratio->GetYaxis()->SetTitleSize(0.1);
+  ratio->GetYaxis()->SetTitleOffset(0.4);
+  ratio->GetXaxis()->SetTitleOffset(0.65);
+  ratio->GetYaxis()->SetNdivisions(606, 3);
+  ratio->Draw("pe");
+
+  ratioWeight->SetLineColor(kGreen + 2);
+  ratioWeight->SetMarkerColor(kGreen + 2);
+  ratioWeight->GetYaxis()->SetRangeUser(0.5, 1.28);
+  ratioWeight->GetXaxis()->SetLabelSize(0.1);
+  ratioWeight->GetYaxis()->SetLabelSize(0.1);
+  ratioWeight->GetYaxis()->SetTitleSize(0.1);
+  ratioWeight->GetYaxis()->SetTitleOffset(0.4);
+  ratioWeight->GetXaxis()->SetTitleOffset(0.6);
+  ratioWeight->GetYaxis()->SetNdivisions(505, 2);
+  ratioWeight->Draw("hist, same");
 }
 
 void tupleReader() {
@@ -89,90 +151,116 @@ void tupleReader() {
   mcFile->GetObject("osc_tuple", mcTuple);
   dataFile->GetObject("osc_tuple", dataTuple);
 
+  double mcLivetime = 182625.0; // eq 500years sk4 // 1851250
+  double dataLivetime = 3244.4; // sk4 fcdata // 30621
+  double livetimeRatio = (double)1.07 * dataLivetime / mcLivetime;
+
+  TString eCondition = "wall > 200 && nring == 1 && ip == 2 ";
+  TString muCondition = "wall > 200 && nring == 1 && ip == 3 ";
+
   ///////////////////////////////////////
-  TString eDataCondition = "amom > 400 && wall > 200 && nring == 1 && ip == 2";
-  TH1F *eDatahist = getAngleDistribution(dataTuple, eDataCondition);
-  setPlottingOptions(eDatahist, "e data; cos#theta; counts"); //" + eDataCondition + "
 
-  TString eMCCondition = 
-      "pnu > 0.4 && wall > 200 && nring == 1 && (mode == 1 || mode == -1) && "
-      "ip == 2 && (ipnu == 12 || ipnu == -12)"; //&& itype == 59
-  TH1F *eMChistWeight = getAngleDistribution(mcTuple, eMCCondition);
-  setPlottingOptions(eMChistWeight, "e-like " + eDataCondition + "; cos#theta; counts");
-  eMChistWeight->Scale(eDatahist->Integral() / eMChistWeight->Integral());
+  TH1F *eDatahist =
+      getAngleDistribution(dataTuple, eCondition + "&& amom < 400", false);
+      setPlottingOptions(eDatahist, "; ; ");
 
-  TH1F *eMChist = getAngleDistribution(mcTuple, eMCCondition, false);
-  setPlottingOptions(eMChist, "e-like; cos#theta; counts");
-  eMChist->Scale(eDatahist->Integral() / eMChist->Integral());
+  TH1F *eMChistWeight =
+      getAngleDistribution(mcTuple, eCondition + "&& amom < 400");
+      setPlottingOptions(eMChistWeight, "e-like; ; events");
+      eMChistWeight->GetYaxis()->SetTitleSize(0.05);
+  eMChistWeight->Scale(livetimeRatio);
+
+  TH1F *eMChist =
+      getAngleDistribution(mcTuple, eCondition + "&& amom < 400", false);
+      setPlottingOptions(eMChist, "; ; ");
+  eMChist->Scale(livetimeRatio);
 
   ////////////////////////////////////////
-  TString muDataCondition = "amom > 400 && wall > 100 && nring == 1 && ip == 3";
-  TH1F *muDatahist = getAngleDistribution(dataTuple, muDataCondition);
-  setPlottingOptions(muDatahist, "#mu-like; cos#theta; counts"); //" + muDataCondition + "
+  TH1F *muDatahist =
+      getAngleDistribution(dataTuple, muCondition + "&& amom < 400", false);
+      setPlottingOptions(muDatahist, "; ; ");
 
-  TString muMCCondition =
-      "pnu > 0.4 && wall > 100 && nring == 1 && (mode == 1 || mode == -1) && "
-      "ip == 3 && (ipnu == 14 || ipnu == -14)"; //&& itype == 63
-  TH1F *muMChistWeight = getAngleDistribution(mcTuple, muMCCondition);
-  setPlottingOptions(muMChistWeight, "#mu-like " + muDataCondition + "; cos#theta; counts"); //" + muMCCondition +"
-  muMChistWeight->Scale(muDatahist->Integral() / muMChistWeight->Integral());
+  TH1F *muMChistWeight =
+      getAngleDistribution(mcTuple, muCondition + "&& amom < 400");
+      setPlottingOptions(muMChistWeight, "#mu-like; ; events");
+      muMChistWeight->GetYaxis()->SetTitleSize(0.05);
+  muMChistWeight->Scale(livetimeRatio);
 
-  TH1F *muMChist = getAngleDistribution(mcTuple, muMCCondition, false);
-  setPlottingOptions(muMChist, "#mu-like; cos#theta; counts");
-  muMChist->Scale(muDatahist->Integral() / muMChist->Integral());
-  ////////////////
-
-  TH1F* mcRatio = (TH1F*)muMChist->Clone();
-  mcRatio->Divide(eMChist);
-
-  TH1F* dataRatio = (TH1F*)muDatahist->Clone();
-  dataRatio->Divide(eDatahist);
-
-  TH1F* doubleRatio = (TH1F*)dataRatio->Clone();
-  doubleRatio->Divide(mcRatio);
-  setPlottingOptions(doubleRatio, "R = #frac{(N_{#mu}/N_{e})_{data}}{(N_{#mu}/N_{e})_{MC}} ; cos#theta; R");
-
-  TH1F* muRatio = (TH1F*)muDatahist->Clone();
-  muRatio->Divide(muMChist);
-  setPlottingOptions(muRatio, "#mu -like ; cos#theta; data/MC");
-
-  TH1F* eRatio = (TH1F*)eDatahist->Clone();
-  eRatio->Divide(eMChist);
-  setPlottingOptions(eRatio, "e-like; cos#theta; data/MC");
+  TH1F *muMChist =
+      getAngleDistribution(mcTuple, muCondition + "&& amom < 400", false);
+      setPlottingOptions(muMChist, "; ; ");
+  muMChist->Scale(livetimeRatio * 0.89);
 
   ///////////////////////////////
-  TCanvas *canvas = new TCanvas("canvas1", "", 800, 600);
-  canvas->Divide(2, 2);
+  //       OVER 400 MeV/c      //
+  ///////////////////////////////
+
+  TH1F *eDatahistOver400MeV =
+      getAngleDistribution(dataTuple, eCondition + "&& amom > 400", false);
+      setPlottingOptions(eDatahistOver400MeV, "; cos#theta; ");
+
+  TH1F *eMChistWeightOver400MeV =
+      getAngleDistribution(mcTuple, eCondition + "&& amom > 400");
+      setPlottingOptions(eMChistWeightOver400MeV, "; cos#theta; events");
+      eMChistWeightOver400MeV->GetYaxis()->SetTitleSize(0.04);
+  eMChistWeightOver400MeV->Scale(livetimeRatio);
+
+  TH1F *eMChistOver400MeV =
+      getAngleDistribution(mcTuple, eCondition + "&& amom > 400", false);
+      setPlottingOptions(eMChistOver400MeV, "; cos#theta; ");
+  eMChistOver400MeV->Scale(livetimeRatio);
+
+  ////////////////////////////////////////
+
+  TH1F *muDatahistOver400MeV =
+      getAngleDistribution(dataTuple, muCondition + "&& amom > 400", false);
+      setPlottingOptions(muDatahistOver400MeV, "; cos#theta; ");
+
+  TH1F *muMChistWeightOver400MeV =
+      getAngleDistribution(mcTuple, muCondition + "&& amom > 400");
+      setPlottingOptions(muMChistWeightOver400MeV, "; cos#theta; events");
+      muMChistWeightOver400MeV->GetYaxis()->SetTitleSize(0.04);
+  muMChistWeightOver400MeV->Scale(livetimeRatio);
+
+  TH1F *muMChistOver400MeV =
+      getAngleDistribution(mcTuple, muCondition + "&& amom > 400", false);
+      setPlottingOptions(muMChistOver400MeV, "; cos#theta; ");
+  muMChistOver400MeV->Scale(livetimeRatio); 
+
+  ///////////////////////////////
+
+  TCanvas *canvas = new TCanvas("canvas", "", 800, 700);
+  canvas->Divide(2, 2, 0, 0);
+
+  ///////////////////////////////
 
   canvas->cd(1);
-  eMChistWeight->SetLineColor(kGreen + 2);
-  eMChistWeight->Draw("hist");
-  eMChist->SetLineColor(kRed);
-  eMChist->Draw("hist, same");
-  eDatahist->SetLineColor(kBlack);
-  eDatahist->SetMarkerStyle(20);
-  eDatahist->SetMarkerSize(0.8);
-  eDatahist->Draw("PE, same");
+  plotOnePad(eDatahist, eMChist, eMChistWeight, "p < 400 MeV/c");
 
   canvas->cd(2);
-  muMChistWeight->SetLineColor(kGreen + 2);
-  muMChistWeight->Draw("hist");
-  muMChist->SetLineColor(kRed);
-  muMChist->Draw("hist, same");
-  muDatahist->SetLineColor(kBlack);
-  muDatahist->SetMarkerStyle(20);
-  muDatahist->SetMarkerSize(0.8);
-  muDatahist->Draw("PE, same");
+  plotOnePad(muDatahist, muMChist, muMChistWeight, "p < 400 MeV/c");
+
+  canvas->cd(1);
+  plotOneRatio(eDatahist, eMChist, eMChistWeight);
+
+  canvas->cd(2);
+  plotOneRatio(muDatahist, muMChist, muMChistWeight);
+
+
+  ///////////////////////////////
+  //       OVER 400 MeV/c      //
+  ///////////////////////////////
 
   canvas->cd(3);
-  eRatio->Draw("pe");
+  plotOnePad(eDatahistOver400MeV, eMChistOver400MeV, eMChistWeightOver400MeV, "p > 400 MeV/c");
 
   canvas->cd(4);
-  muRatio->Draw("pe");
-  TLine *line = new TLine(-1, 1, 1, 1);
-  line->SetLineColor ( kBlack );
-  line->SetLineWidth (1);
-  line->Draw("same");
+  plotOnePad(muDatahistOver400MeV, muMChistOver400MeV, muMChistWeightOver400MeV, "p > 400 MeV/c");
 
+  canvas->cd(3);
+  plotOneRatio(eDatahistOver400MeV, eMChistOver400MeV, eMChistWeightOver400MeV);
+
+  canvas->cd(4);
+  plotOneRatio(muDatahistOver400MeV, muMChistOver400MeV, muMChistWeightOver400MeV);
 
 }
